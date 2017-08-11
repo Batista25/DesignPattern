c2f9d00b7ce6fe5b643d2df8c8f07711640ddb6d
chenshi <chenshi@boe.com.cn>
2017-08-11 14:30:04 +0800

    本地提交

---
+++ src/com/chen/pattern/Strategy/Strategy.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.chen.pattern.Strategy;
+
+/**
+ * @author ChenShi
+ * @date   2017年8月11日上午10:48:46
+ */
+public class Strategy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
